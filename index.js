df8a55f74d5073a134186611db0ca3c87bc5aaf9
Vladimir Herdman <vovaherdman@gmail.com>
2025-03-15 14:20:04 -0400

    Corrected card mouse tracking, added education and experiences section

--- index.js
+++ index.js
@@ -16,12 +16,19 @@ document.getElementById("overview").addEventListener("mouseleave", (e) => {
 function rotateElement(event, element) {
 	const x = event.clientX;
 	const y = event.clientY;
+	
+	// base card movement off position relative to overview section and its size
+	const overview_rect = document.getElementById("overview").getBoundingClientRect();
+	let middleX;
+	if (window.innerWidth <= 1180) {
+		middleX = overview_rect.left + overview_rect.width / 2;
+	} else {
+		middleX = overview_rect.left + overview_rect.width / 4;
+	}
+	const middleY = overview_rect.bottom - overview_rect.height / 2;
 
-	const middleX = window.innerWidth / 2;
-	const middleY = window.innerHeight / 2;
-
-	const offsetX = ((x - middleX) / middleX) * 40;
-	const offsetY = ((y - middleY) / middleY) * 40;
+	const offsetX = ((x - middleX) / middleX) * 30;
+	const offsetY = ((y - middleY) / middleY) * 30;
 
 	element.style.setProperty("--rotateX", -1 * offsetY + "deg");
 	element.style.setProperty("--rotateY", offsetX + "deg");
